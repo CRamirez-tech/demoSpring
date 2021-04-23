a5beb40572a67476caf4771dbb23c8ccafdd4671
CRamirez-tech <cramirez@unsa.edu.pe>
2021-04-22 21:59:54 -0500

    second commit :D funciona

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -1,18 +1,20 @@
-package com.example.controller;
+package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class UserController {
 
 	@GetMapping("/")
 	public String index() {
-		return "index.html";
+		return "index";
 	}
 	
 	@GetMapping("/userForm")
 	public String getUserForm() {
-		return "user-form/user-view.html";
+		return "user-form/user-view";
 	}	
 }
